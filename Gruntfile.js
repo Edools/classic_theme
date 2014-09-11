1b83e5a06928d26a3a38125d4cda448a2643ac82
Diogo Beda <diogo.fbeda@gmail.com>
2014-09-11 11:00:19 -0300

    using new grunt-edools-server version

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,6 @@ module.exports = function (grunt) {
 
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
-  grunt.loadNpmTasks('edools-grunt-server');
 
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
