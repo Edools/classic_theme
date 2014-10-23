46b34fa999ea659a3ea77b83e2b6190d565bb3cc
Diogo Beda <diogo.fbeda@gmail.com>
2014-10-23 11:28:29 -0200

    adding demo credentials

--- Gruntfile.js
+++ Gruntfile.js
@@ -365,9 +365,9 @@ module.exports = function (grunt) {
 
     edools_deploy: {
       options: {
-        domain: 'your_domain',
-        theme: 'your_theme_id',
-        token: 'your_authorization_token',
+        domain: 'demo',
+        theme: '53ab222e72616969ed000000',
+        token: 'b40bbce82ef79ea6be0fb3294e7d948c:b712a32e4fb59e141e935da4c79ebce1',
         package_file: '<%= theme.public %>/<%= pkg.name %>.zip'
       },
       dist: {
